57ebf2c550524d6e2ade8942e546319161cc9a1f
Ethan Pierce <ethan.pierce@colorado.edu>
2024-06-11 10:54:10 -0600

    Make Fields check grid location on init

--- glacierbento/utils/core.py
+++ glacierbento/utils/core.py
@@ -10,10 +10,19 @@ import equinox as eqx
 
 
 class Field(eqx.Module):
-    """Stores spatially variable data."""
+    """Stores spatially variable data.
+    
+    Attributes:
+        name: A human-readable string identifier for the field.
+        value: An array-like object containing the data.
+        units: A string indicating the units of the data.
+        location: A string indicating the location of the data.
+    """
     name: str = eqx.field(converter = str)
     value: jnp.ndarray = eqx.field(converter = jnp.asarray)
     units: str = eqx.field(converter = str)
     location: str = eqx.field(converter = str)
 
-
+    def __post_init__(self):
+        if location not in ["node", "link", "patch", "corner", "face", "cell"]:
+            raise ValueError("Invalid location for field.")
